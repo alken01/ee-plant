7be2acb487ade93e83fa23cbd53163e0d5106aa8
tuur.mellaerts <tuur.mellaerts@student.kuleuven.be>
2022-03-08 15:00:56 +0100

    Made updates

--- ESP32/adc_sensors/adc_WaterLevelSensor.c
+++
@@ -1,9 +0,0 @@
-// ADC for water level sensor
-
-#include <stdio.h>
-#include <stdlib.h>
-
-void adc_WaterLevelSensor(void)
-{
-    printf("It works!");
-}
--- ESP32/main/main.c
+++
@@ -1,8 +0,0 @@
-// Main c-file
-
-#include "../adc_sensors/adc_WaterLevelSensor.h"
-
-void main(void)
-{
-    adc_WaterLevelSensor();
-}
