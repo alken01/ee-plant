014ed7aff319b8a526136dbf0babcfcd3b5692f6
tuur.mellaerts <tuur.mellaerts@student.kuleuven.be>
2022-03-29 15:07:44 +0200

    Update

--- ESP32/c-files/main/adc_read.h
+++
@@ -1,8 +0,0 @@
-void config_read_light(void);
-void config_read_temp(void);
-void reset_read_light(void);
-void reset_read_temp(void);
-void config_adc_temperature(void);
-void config_adc_light(void);
-void adc_read(void);
-
--- ESP32/c-files/main/temperature_read.h
+++
@@ -1,3 +0,0 @@
-
-
-void temperature_read(void);
